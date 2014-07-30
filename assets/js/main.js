f3d9f3c8f5bc07d19c07bf28de7d94eeb62612ea
Ashutosh Rishi Ranjan <rishi@Ashutoshs-MacBook-Air.local>
2014-07-30 23:29:14 +1000

    Blub

--- assets/js/main.js
+++
@@ -1,9 +0,0 @@
-
-
-$(document).ready(function() {
-    if( /Android/i.test(navigator.userAgent) ) {
-        $("#jumper").html("I love you, Gina :) <span class='glyphicon glyphicon-heart'></span>");
-    }
-});
-
-
