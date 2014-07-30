e901406573db110bc081d735016d3f2f1fb42e09
Ashutosh Rishi Ranjan <rishi@Ashutoshs-MacBook-Air.local>
2014-07-30 23:17:56 +1000

    JS files

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+
+
+$(document).ready(function() {
+    if( /Android/i.test(navigator.userAgent) ) {
+        $("#jumper").html("I love you, Gina :) <span class='glyphicon glyphicon-heart'></span>");
+    }
+});
+
+
