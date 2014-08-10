1c4f4a5b6c8d911e5f6b2c1d585c9fd4fb83cafb
Ashutosh Rishi Ranjan <rishi@Rishis-MacBook-Air.local>
2014-08-11 01:42:30 +1000

    Added links correctly.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,3 @@
 (function() {
-    $(document).ready(function() {
-        $("#about-link").click(function () {
-            $(this).find("#hidden-about").show("slow", function () {
-                // Complete animation
-            });
-        });
-    });
+
 }());
