87a4deb42d6a0e1d9382246a272106f0f744ee68
Ashutosh Rishi Ranjan <rishi@Ashutoshs-MacBook-Air.local>
2014-11-18 02:19:28 +1100

    Added a owl carousel for books section.

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,5 +2,29 @@ $(document).ready(function() {
     $(".menu-button").click(function(e) {
         e.preventDefault();
         $("#wrapper").toggleClass("active");
-    });    
+    });
+
+    // Owl Carousel
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        center: true,
+        dots: true,
+        responsiveClass:true,
+        URLhashListener:true,
+        responsive:{
+            0:{
+                items:2,
+                center: true                
+            },
+            600:{
+                items:4,
+                center: true                
+            },
+            1000:{
+                items:5,
+                center: true
+            }
+        }
+    })
+    
 });
